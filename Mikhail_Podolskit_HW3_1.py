115f80c955a0c8eb08adf19f6144fd20da8dabb4
Mikhail Podolskiy <testofprices@yandex.ru>
2023-06-16 23:29:12 +0300

    Add 1st zadachu

--- Mikhail_Podolskit_HW3_1.py
+++ Mikhail_Podolskit_HW3_1.py
@@ -1,3 +1,104 @@
+print("Задача №1")
+
+class Auto:
+    def __init__(self, year, model):
+        self.year = year
+        self.model = model
+
+    def get_year(self):
+        return self.year
+
+    def get_model(self):
+        return self.model
+
+    def tech_info(self):
+        print(f"\nГод выпуска: {self.year}; Модель: {self.model}")
+
+    def move(self):
+        print("Помчался....")
+
+a1 = Auto(1978, "Ваз 2109")
+
+a1.tech_info()
+a1.move()
+print(a1.get_model())
+print(a1.get_year())
+
+
+class Train:
+    def __init__(self, year, model):
+        self.year = year
+        self.model = model
+
+    def get_year(self):
+        return self.year
+
+    def get_model(self):
+        return self.model
+
+    def tech_info(self):
+        print(f"\nГод выпуска: {self.year}; Модель: {self.model}")
+
+    def hard_stop(self):
+        print("Жесткая остановка....")
+
+t1 = Train(1999, "Ласточка")
+
+t1.tech_info()
+t1.hard_stop()
+print(t1.get_model())
+print(t1.get_year())
+
+class Transport_sredstvo:
+    def __init__(self, year, model):
+        self.year = year
+        self.model = model
+
+    def get_year(self):
+        return self.year
+
+    def get_model(self):
+        return self.model
+
+    def tech_info(self):
+        print(f"\nГод выпуска: {self.year}; Модель: {self.model}")
+
+    def yamzhik(self):
+        print("Ямщик не гони лошадей....")
+
+ts1 = Transport_sredstvo(1978, "Тройка лошадей")
+
+ts1.tech_info()
+ts1.yamzhik()
+print(ts1.get_model())
+print(ts1.get_year())
+
+class Express:
+    def __init__(self, year, model):
+        self.year = year
+        self.model = model
+
+    def get_year(self):
+        return self.year
+
+    def get_model(self):
+        return self.model
+
+    def tech_info(self):
+        print(f"\nГод выпуска: {self.year}; Модель: {self.model}")
+
+    def moving(self):
+        print("Помчали с ветерком....")
+
+ex1 = Express(2015, "Сапсан")
+
+ex1.tech_info()
+ex1.moving()
+print(ex1.get_model())
+print(ex1.get_year())
+
+
+
 print(f"\n Задача №2")
 class Rectangle:
     def __init__(self, dlina, shirina):
